d3d002467649b34df19a5b5be335e98f789e267c
Rob Cone <robcone86@gmail.com>
2021-10-29 21:02:07 -0500

    home screen is up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import axios from 'axios';
+import {HashRouter as Router, Route} from 'react-router-dom'
 import './App.css';
+
+import Feeling1 from '../Feeling1/Feeling1';
+// import Understanding2 from '../Understanding2/Understanding2';
+// import Supported3 from '../Supported3/Supported3';
+// import Comments4 from '..Comments4/Comments4';
+// import Review5 from '..Review5/Review5';
+// import Success6 from '../Success6/Success6';
+
+
 
 function App() {
 
@@ -10,6 +20,13 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+    <Router>
+    
+    <Route path="/" exact>
+      <Feeling1 />
+    </Route>
+    
+    </Router>
     </div>
   );
 }
---
+++ src/components/Feeling1/Feeling1.jsx
@@ -0,0 +1,36 @@
+import { useHistory } from "react-router";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+
+function Feeling1() {
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [feeling, setFeeling] = useState();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (feeling > 0 && feeling <= 5) {
+            dispatch({ type: 'FEELING', payload: feeling });
+            history.push('/understanding2');
+        } else {
+            alert('Please enter a number 1 - 5');
+        }
+    };
+return (
+<div>
+    <h2>How are you feeling today?</h2>
+    <form onSubmit={handleSubmit}>
+        <input
+        type="text"
+        value={feeling}
+        onChange={event => setFeeling(event.target.value)}
+        />
+        <button type="submit">NEXT</button>
+    </form>
+</div>
+);
+};
+
+
+export default Feeling1
